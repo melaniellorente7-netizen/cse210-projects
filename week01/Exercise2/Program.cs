9192d221b35808430262bf81b7e17a3521af208f
melaniellorente7-netizen <mllorentetellechea@byupathway.edu>
2026-07-04 01:56:38 -0300

    5 exercises done for week 01

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter the grade percentage: ");
+        string userInput = Console.ReadLine();
+        float grade = float.Parse(userInput);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >=80)
+        {
+            letter = "B";
+        }
+        else if (grade >=70)
+        {
+            letter = "C";
+        }
+        else if (grade >=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (grade >=70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Try again next time!");
+        }
+
     }
 }
